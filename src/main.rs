0ff1ad1e0fa279508f17823a640d35f926a36eeb
dyoshimitsu <contact@dyoshimitsu.com>
2023-03-15 15:23:38 +0900

    add body

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 use std::env;
+use serde_json::json;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://api.openai.com/v1/chat/completions";
     let client = reqwest::Client::new();
 
+    let url = "https://api.openai.com/v1/chat/completions";
     let bearer_auth = env::var("OPENAI_API_KEY");
+
+    let body = json!({
+        "model": "gpt-3.5-turbo",
+        "messages": [{
+            "role": "user",
+            "content": "What is the OpenAI mission?",
+        }],
+    });
 
     match bearer_auth {
         Ok(bearer_auth) => {
-            let res = client.get(url)
+            let res = client.post(url)
                 .header("Content-Type", "application/json")
                 .bearer_auth(bearer_auth)
+                .body(body.to_string())
                 .send()
                 .await?
                 .text()
