d785024a864ffc31fb047efcfc1b311917088c6d
dyoshimitsu <contact@dyoshimitsu.com>
2023-03-15 12:25:15 +0900

    add io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();  // 標準入力を格納する文字列変数を作成する
+    io::stdin().read_line(&mut input).expect("Failed to read line");  // 標準入力から文字列を読み込む
+
+    println!("You typed: {}", input);  // 読み込んだ文字列を表示する
 }
