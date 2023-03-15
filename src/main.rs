0298ab8090b93a0f3095c298c32c24044d96fcf7
dyoshimitsu <contact@dyoshimitsu.com>
2023-03-15 14:42:54 +0900

    add env

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
+use std::env;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://api.openai.com/v1/chat/completions";
     let client = reqwest::Client::new();
 
-    let res = client.get(url)
-        .send()
-        .await?
-        .text()
-        .await?;
+    let bearer_auth = env::var("OPENAI_API_KEY");
 
-    println!("{}", res);
+    match bearer_auth {
+        Ok(bearer_auth) => {
+            let res = client.get(url)
+                .header("Content-Type", "application/json")
+                .bearer_auth(bearer_auth)
+                .send()
+                .await?
+                .text()
+                .await?;
+
+            println!("{}", res);
+        }
+        Err(e) => println!("OPENAI_API_KEYが設定されていません: {:?}", e),
+    }
+
     Ok(())
 }
